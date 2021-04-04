7b3174cc3fd633b69b191ef3edd9c7ab5f7eba5e
mTvare <57838468+mTvare6@users.noreply.github.com>
2021-04-04 18:45:26 +0530

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,12 @@
+git clone https://github.com/mirage-apps/cli
+cd cli
+mkdir bin
+cd bin
+fsutil file createnew mirage 0
+cd ..
+make clean
+make binary
+mv C:/PROGRAM FILES AHHHH
+cd ..
+mkdir %HOMEPATH%/.mirage-store
+rd /s /q "cli"
